792d565348dab5d377149e35d062d7cfc97bf6f1
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-04 21:38:41 +0200

    create repository interface and inject it to service

--- projekt/Spring-Restaurant-Microservice/Menu-Service/src/main/java/com/example/menuservice/controller/MenuController.java
+++ projekt/Spring-Restaurant-Microservice/Menu-Service/src/main/java/com/example/menuservice/controller/MenuController.java
@@ -11,4 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MenuController {
 
     private final MenuService menuService;
+
+
 }
--- projekt/Spring-Restaurant-Microservice/Menu-Service/src/main/java/com/example/menuservice/service/MenuService.java
+++ projekt/Spring-Restaurant-Microservice/Menu-Service/src/main/java/com/example/menuservice/service/MenuService.java
@@ -1,7 +1,12 @@
 package com.example.menuservice.service;
 
+import com.example.menuservice.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MenuService {
+
+    private final MenuRepository menuRepository;
 }
