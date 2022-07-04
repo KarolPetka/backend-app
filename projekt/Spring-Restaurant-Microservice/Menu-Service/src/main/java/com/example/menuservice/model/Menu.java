c3c1a9b2a82156ddb2c04a612d7f0e3561311bb1
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-04 20:53:25 +0200

    add fields to menu model class

--- projekt/Spring-Restaurant-Microservice/Menu-Service/src/main/java/com/example/menuservice/model/Menu.java
+++ projekt/Spring-Restaurant-Microservice/Menu-Service/src/main/java/com/example/menuservice/model/Menu.java
@@ -1,4 +1,20 @@
 package com.example.menuservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Menu {
+    @Id
+    private Long id;
+    private String dish;
+    private String description;
+    private int price;
 }
