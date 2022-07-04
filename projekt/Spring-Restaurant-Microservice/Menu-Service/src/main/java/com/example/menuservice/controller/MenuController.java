84b0e2955d05f69f046957f48184ffd9e56a702e
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-04 21:33:11 +0200

    create service class and inject it to controller

--- projekt/Spring-Restaurant-Microservice/Menu-Service/src/main/java/com/example/menuservice/controller/MenuController.java
+++ projekt/Spring-Restaurant-Microservice/Menu-Service/src/main/java/com/example/menuservice/controller/MenuController.java
@@ -1,9 +1,14 @@
 package com.example.menuservice.controller;
 
+import com.example.menuservice.service.MenuService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/menu")
+@RequiredArgsConstructor
 public class MenuController {
+
+    private final MenuService menuService;
 }
