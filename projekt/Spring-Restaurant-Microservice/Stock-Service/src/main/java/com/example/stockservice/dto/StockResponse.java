40e48ba2fe3c845fbad5bec7073d9f8e583f91df
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-05 22:46:37 +0200

    implemented controller for stock class

---
+++ projekt/Spring-Restaurant-Microservice/Stock-Service/src/main/java/com/example/stockservice/dto/StockResponse.java
@@ -0,0 +1,16 @@
+package com.example.stockservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockResponse {
+    private Long id;
+    private String dish;
+    private int quantity;
+}
