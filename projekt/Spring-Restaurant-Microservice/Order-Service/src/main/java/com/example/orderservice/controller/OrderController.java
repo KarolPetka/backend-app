33688a67d7283bea5fccd2060261effd733ab017
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-05 18:58:33 +0200

    create order service and inject it into controller

--- projekt/Spring-Restaurant-Microservice/Order-Service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ projekt/Spring-Restaurant-Microservice/Order-Service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.example.orderservice.controller;
 
+import com.example.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
+
+    public final OrderService orderService;
 }
