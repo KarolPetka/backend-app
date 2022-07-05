a23053ad47e94a836b33ca00965c71b7f5d7033b
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-05 23:08:17 +0200

    implement stock repository and injected it to service

--- projekt/Spring-Restaurant-Microservice/Stock-Service/src/main/java/com/example/stockservice/service/StockService.java
+++ projekt/Spring-Restaurant-Microservice/Stock-Service/src/main/java/com/example/stockservice/service/StockService.java
@@ -1,7 +1,12 @@
 package com.example.stockservice.service;
 
+import com.example.stockservice.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StockService {
+
+    private final StockRepository stockRepository;
 }
