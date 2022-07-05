96896512db90f14ddee051f85e5a12a2e4a2f2cc
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-05 23:15:58 +0200

    implement GET HTTP handling

--- projekt/Spring-Restaurant-Microservice/Stock-Service/src/main/java/com/example/stockservice/controller/StockController.java
+++ projekt/Spring-Restaurant-Microservice/Stock-Service/src/main/java/com/example/stockservice/controller/StockController.java
@@ -1,9 +1,15 @@
 package com.example.stockservice.controller;
 
+import com.example.stockservice.model.Stock;
 import com.example.stockservice.service.StockService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/stock")
@@ -11,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class StockController {
 
     private final StockService stockService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Stock> getStock(){
+        return stockService.getStock();
+    }
 }
--- projekt/Spring-Restaurant-Microservice/Stock-Service/src/main/java/com/example/stockservice/service/StockService.java
+++ projekt/Spring-Restaurant-Microservice/Stock-Service/src/main/java/com/example/stockservice/service/StockService.java
@@ -1,12 +1,19 @@
 package com.example.stockservice.service;
 
+import com.example.stockservice.model.Stock;
 import com.example.stockservice.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class StockService {
 
     private final StockRepository stockRepository;
+
+    public List<Stock> getStock() {
+        return stockRepository.findAll();
+    }
 }
