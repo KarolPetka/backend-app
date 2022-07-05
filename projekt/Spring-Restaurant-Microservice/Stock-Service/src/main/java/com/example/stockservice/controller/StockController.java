dab8df94fe323b5149a99bb02052434160e4cf0b
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-05 23:04:15 +0200

    Implement stock service and injected it to controller

--- projekt/Spring-Restaurant-Microservice/Stock-Service/src/main/java/com/example/stockservice/controller/StockController.java
+++ projekt/Spring-Restaurant-Microservice/Stock-Service/src/main/java/com/example/stockservice/controller/StockController.java
@@ -1,9 +1,14 @@
 package com.example.stockservice.controller;
 
+import com.example.stockservice.service.StockService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/stock")
+@RequiredArgsConstructor
 public class StockController {
+
+    private final StockService stockService;
 }
