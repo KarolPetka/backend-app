8731234427fd02c7554ea0b1c3c689b637ce853c
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-05 18:49:53 +0200

    add model classes for order service

---
+++ projekt/Spring-Restaurant-Microservice/Order-Service/src/main/java/com/example/orderservice/model/Order.java
@@ -0,0 +1,23 @@
+package com.example.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderItems> orderItems;
+}
