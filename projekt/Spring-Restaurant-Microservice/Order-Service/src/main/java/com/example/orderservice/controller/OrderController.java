66763b567e9a89132454abe8ddfd09ecf4576d36
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-05 20:10:14 +0200

    add GET HTTP handling for orders

--- projekt/Spring-Restaurant-Microservice/Order-Service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ projekt/Spring-Restaurant-Microservice/Order-Service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,9 +1,15 @@
 package com.example.orderservice.controller;
 
+import com.example.orderservice.model.Order;
 import com.example.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/order")
@@ -12,5 +18,9 @@ public class OrderController {
 
     public final OrderService orderService;
 
-
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Order> getOrders(){
+        return orderService.getOrders();
+    }
 }
--- projekt/Spring-Restaurant-Microservice/Order-Service/src/main/java/com/example/orderservice/service/OrderService.java
+++ projekt/Spring-Restaurant-Microservice/Order-Service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -1,12 +1,19 @@
 package com.example.orderservice.service;
 
+import com.example.orderservice.model.Order;
 import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    public List<Order> getOrders() {
+        return orderRepository.findAll();
+    }
 }
