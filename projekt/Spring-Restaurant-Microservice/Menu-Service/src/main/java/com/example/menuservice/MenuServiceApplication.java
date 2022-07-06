dcbd03dae1e01bf926c2a4a205b700d74df4bdd0
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-06 11:22:57 +0200

    implemented discovery server

--- projekt/Spring-Restaurant-Microservice/Menu-Service/src/main/java/com/example/menuservice/MenuServiceApplication.java
+++ projekt/Spring-Restaurant-Microservice/Menu-Service/src/main/java/com/example/menuservice/MenuServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.menuservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MenuServiceApplication {
 
 	public static void main(String[] args) {
