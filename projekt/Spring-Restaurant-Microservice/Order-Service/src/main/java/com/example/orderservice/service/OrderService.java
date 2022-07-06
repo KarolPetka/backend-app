cdaa64cc5f19b989d277a5ed3b5a37751d08dea8
Karol Petka <60034422+KarolPetka@users.noreply.github.com>
2022-07-06 18:56:28 +0200

    inject webclient into order service

--- projekt/Spring-Restaurant-Microservice/Order-Service/src/main/java/com/example/orderservice/service/OrderService.java
+++ projekt/Spring-Restaurant-Microservice/Order-Service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -5,6 +5,7 @@ import com.example.orderservice.model.Order;
 import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public List<Order> getOrders() {
         return orderRepository.findAll();
